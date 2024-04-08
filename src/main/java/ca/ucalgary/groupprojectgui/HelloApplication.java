a9bb4c3ab518398e6ff3b331a388ba56dcd1f923
Dipti <dipti.kumar@ucalgary.ca>
2024-04-07 19:14:39 -0600

    Course constructor added

--- src/main/java/ca/ucalgary/groupprojectgui/HelloApplication.java
+++ src/main/java/ca/ucalgary/groupprojectgui/HelloApplication.java
@@ -25,8 +25,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 620, 440);
+        stage.setTitle("Academic Tracker v" + version);
         stage.setScene(scene);
         stage.show();
     }
